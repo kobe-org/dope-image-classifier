7f474eb8b9c0bb4e81be4bf498df27882d15c4f5
simon-benigeri <simon.benigeri@protonmail.ch>
2021-09-11 19:00:53 +0200

    add env and python files

---
+++ train.py
@@ -0,0 +1,99 @@
+from pathlib import Path
+
+import torch
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import typer
+from fastai.vision.all import *
+from PIL import ImageFile
+from tqdm import tqdm
+
+from model import Net
+
+
+def train(model: Net, epochs: int, learning_rate: float, momentum: float, trainloader: DataLoader):
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+
+    for epoch in tqdm(range(epochs)):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+        
+    return model
+
+
+def main(
+    image_folder: Path = typer.Option(...),
+    save_to_folder: Path = typer.Option(...),
+    epochs: int = 5,
+    learning_rate: float = 0.001,
+    momentum: float = 0.9,
+    batch_size: int = 64
+):
+    """[summary]
+
+    Args:
+        image_folder (Path, optional): [description]. Defaults to typer.Option(...).
+        save_to_folder (Path, optional): [description]. Defaults to typer.Option(...).
+        epochs (int, optional): [description]. Defaults to 5.
+        learning_rate (float, optional): [description]. Defaults to 0.001.
+        momentum (float, optional): [description]. Defaults to 0.001.
+        batch_size (int, optional): [description]. Defaults to 64.
+
+    Returns:
+        [type]: [description]
+    """
+    image_folder.mkdir(parents=True, exist_ok=True)
+    save_to_folder.mkdir(parents=True, exist_ok=True)
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root=image_folder.as_posix(), train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root=image_folder.as_posix(), train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck')
+    model = Net()
+    train(model, epochs, learning_rate, trainloader)
+
+    save_to = save_to_folder / f'cifar10-{int(datetime.now().timestamp())}.pth'
+    torch.save(model.state_dict(), save_to.absolute().as_posix())
+
+    return 0
+
+if __name__ == "__main__":
+    typer.run(main)
---
+++ utils.py
@@ -0,0 +1,32 @@
+import torch
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+
+def imshow(img, batch_size: int, classes: tuple, trainloader: DataLoader):
+    """
+
+    Args:
+        img:
+        batch_size:
+        classes:
+        trainloader:
+
+    Returns:
+
+    """
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
