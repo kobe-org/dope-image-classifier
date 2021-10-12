c10651c2fd10c1099674e952b551f262875c3960
mkobbi <mahmoud.kobbi@hotmail.fr>
2021-10-12 19:13:23 +0100

    WIP #9: Refactored predictions

--- redis_inference.py
+++ redis_inference.py
@@ -1,16 +1,14 @@
+from datetime import timedelta
 from pathlib import Path
-import json
 import os
 import time
+from typing import List
 import numpy as np
 import redisai as rai
 import ml2rt
-from skimage import io
 from skimage.transform import resize
-import typer
 from loguru import logger
-import streamlit as st
-from PIL import Image
+from pydantic import BaseModel
 
 logger.remove()
 logger.add(
@@ -26,13 +24,21 @@ logger.add(
 )
 
 
-def serve_model(
-    gpu: bool,
-    host: str,
-    port: int,
-    model_path: Path,
-    script_path: Path
-):
+class PredictionScore(BaseModel):
+    class_name: str
+    score: float
+
+class Prediction(BaseModel):
+    detail: List[PredictionScore]
+    predicted_class_index: int
+    predicted_class: str
+    processing_time: timedelta
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+def serve_model(gpu: bool, host: str, port: int, model_path: Path, script_path: Path):
 
     device = "gpu" if gpu else "cpu"
 
@@ -46,66 +52,38 @@ def serve_model(
 
 
 def predict(con, image):
-    class_idx = {
-        str(index): class_
-        for index, class_ in enumerate(
-            [
-                "plane",
-                "car",
-                "bird",
-                "cat",
-                "deer",
-                "dog",
-                "frog",
-                "horse",
-                "ship",
-                "truck",
-            ]
-        )
-    }
+    classes = [
+        "plane",
+        "car",
+        "bird",
+        "cat",
+        "deer",
+        "dog",
+        "frog",
+        "horse",
+        "ship",
+        "truck",
+    ]
 
     # image = io.imread(image_path)
     image = resize(image, (32, 32))
 
-    a = time.time()
+    start_processing_time = time.time()
     out3 = con.tensorset("image", image)
     out4 = con.scriptrun("script", "pre_process_3ch", "image", "temp1")
     out5 = con.modelrun("model", "temp1", "temp2")
-    out6 = con.scriptrun("script", "get_score", "temp2", "out")
-    final = con.tensorget("out")
-    print(final)
-    ind = final[0]
-    # logger.info(f'{(ind, class_idx[str(ind)])}\n{time.time() - a}')
-    print(f"{ind}\n{time.time() - a}")
-    return ind
+    out6 = con.scriptrun("script", "get_scores", "temp2", "out")
+    scores = con.tensorget("out").tolist()
+    
+    detailed_scores = [PredictionScore(class_name=key, score=value) for key, value in zip(classes, scores)]
+    predicted_class_index = np.argmax(scores)
+    predicted_class = classes[predicted_class_index]
+    processing_time = timedelta(microseconds=(time.time() - start_processing_time) * 1e6)
 
-
-# def main(
-#     gpu: bool = False,
-#     host: str = "localhost",
-#     port: int = 6379,
-#     model_path: Path = typer.Option(...),
-#     script_path: Path = typer.Option(...),
-#     image_path: Path = typer.Option(...),
-# ):
-model_path = Path(os.path.join(os.path.dirname(__file__), "saved", "cifar10-1632426725.onnx"))
-script_path = Path(os.path.join(os.path.dirname(__file__), "data_processing_script.txt"))
-con = serve_model(False, "localhost", 6379, model_path, script_path)
-
-st.title("Upload + Classification Example")
-
-# file is uploaded as a bytes object
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    # print(uploaded_file.)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
-    st.write("")
-    st.write("Classifying...")
-    label = predict(con, np.array(image))
-    st.write(f'{label}')
-
-
-
-# if __name__ == "__main__":
-#     typer.run(main)
+    prediction = Prediction(
+        detail=detailed_scores,
+        predicted_class_index=predicted_class_index,
+        predicted_class=predicted_class,
+        processing_time=processing_time
+    )
+    return prediction
